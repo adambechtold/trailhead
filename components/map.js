39f6edd65d3fdee6510aa7aff10b6c96e7880914
Adam Bechtold <adamcbechtold@gmail.com>
2023-04-01 17:06:21 -0400

    so we can pinch and zoom the image of the map

--- components/map.js
+++ components/map.js
@@ -1,12 +1,51 @@
-import Image from 'next/image';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+const mapFilePath = '/images/trail-map-smaller.jpeg';
+
+function getMapDimensions(mapFilePath) {
+  const map = new Image();
+  map.src = mapFilePath;
+  const { width, height } = map;
+  return { width, height };
+}
+
+function getViewportDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+}
+
+function getWidthRatio(mapWidth, viewportWidth) {
+  return viewportWidth / mapWidth;
+}
 
 export default function Map() {
-    return (
-        <Image
-            alt="Trail Map"
-            src="/images/trail-map.jpeg"
-            layout="fill"
-            objectFit="contain"
-        />
-    )
+  
+  // TODO: Use this to set the initial scale
+  // I'm not sure why, but something breaks when I try to use it
+  // I can get the ratio, but if I use it to set the initial scale, the map doesn't render correctly
+  // To replicate, uncomment the initialScale prop in the TransformWrapper component below
+  const {
+    width: mapWidth
+  } = getMapDimensions(mapFilePath);
+
+  const {
+    width: viewportWidth,
+  } = getViewportDimensions();
+
+  const widthRatio = getWidthRatio(mapWidth, viewportWidth);
+
+  return (
+    <TransformWrapper
+      limitToBounds={false}
+      initialScale={0.4}
+      // initialScale={widthRatio}
+      minScale={0.3}
+      // minScale={widthRatio * 0.9}
+      maxScale={20}
+    >
+      <TransformComponent>
+        <img src={mapFilePath} alt="Trail Map" />
+      </TransformComponent>
+    </TransformWrapper>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head';
-import Map from '@/components/map';
 import MenuTray from '@/components/menu-tray';
 import Crosshairs from '@/components/crosshairs';
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('@/components/map'), {
+  ssr: false
+});
 
 export default function App() {
+
   return (
     <>
       <Head>
@@ -11,7 +16,6 @@ export default function App() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Map />
-      <Crosshairs />
       <MenuTray />
     </>
   )

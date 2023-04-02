5e7b18d758281b5b75f39136c8e956b69484556b
Adam Bechtold <adamcbechtold@gmail.com>
2023-04-02 09:47:43 -0400

    so we can place pins on the map

--- components/map.js
+++ components/map.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
@@ -7,71 +8,103 @@ import styles from '@/components/map.module.css';
 
 const mapFilePath = '/images/trail-map-smaller.jpeg';
 
-function getMapImageDimensions(mapFilePath) {
-  const map = new Image();
-  map.src = mapFilePath;
-  const { width, height } = map;
-  return { width, height };
-}
 
-function getViewportDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return { width, height };
-}
 
 function getWidthRatio(mapWidth, viewportWidth) {
   return viewportWidth / mapWidth;
 }
 
-export default function Map({ firstPinCorrdinates }) {
+export default function Map({ mapPosition, setMapPosition, pins }) {  
 
-  // TODO: Use this to set the initial scale
-  // I'm not sure why, but something breaks when I try to use it
-  // I can get the ratio, but if I use it to set the initial scale, the map doesn't render correctly
-  // To replicate, uncomment the initialScale prop in the TransformWrapper component below
-  const {
-    width: mapWidth
-  } = getMapImageDimensions(mapFilePath);
-
-  const {
-    width: viewportWidth,
-  } = getViewportDimensions();
-
-  const widthRatio = getWidthRatio(mapWidth, viewportWidth);
-  
   // get the current centerpoint of the map
+  // Example State
+  // {
+  //   positionX: 0,
+  //   positionY: 0,
+  //   scale: 1,
+  //   previousScale: 1,
+  //}
   const [currentMapState, setCurrentMapState] = useState({});
+  const mapReference = useRef(null);
 
-  const handleMapStateUpdate = (state) => { 
-    setCurrentMapState(state);
-    console.log('currentMapState', currentMapState);
+  const handleMapStateUpdate = (state) => {
+    setCurrentMapState(state); // state from the transform component
+    const { positionX, positionY, scale } = state;
+
+    // get the position of the map
+    const mapNode = ReactDOM.findDOMNode(mapReference.current);
+    const mapRect = mapNode.getBoundingClientRect();
+
+    setMapPosition({
+      x: mapRect.left,
+      y: mapRect.top,
+      scale
+    });
   };
+
+
+  const showPin = (pin) => {
+    return (
+      <img
+        src='map-x.svg'
+        alt='Map Pin'
+        className={styles.mapPin}
+        style={{
+          top: pin.top,
+          left: pin.left
+        }}
+        key={`pin-${pin.index}}`}
+      />
+    )
+  }
 
 
   return (
     <TransformWrapper
       limitToBounds={false}
-      initialScale={0.4}
-      // initialScale={widthRatio}
-      minScale={0.3}
-      // minScale={widthRatio * 0.9}
-      maxScale={20}
+      initialScale={mapPosition.scale}
+      minScale={0.3} // TODO: Calculate from image size
+      maxScale={20}  // TODO: Calculate from image size
     >
-      <TransformComponent>
-        <MapStateTracker 
-          setCurrentMapState={handleMapStateUpdate}
-        />
-        <img
-          src='map-pin.svg'
-          alt='Map Pin'
-          className={styles.mapPin}
-          style={{
-            top: firstPinCorrdinates.top,
-            left: firstPinCorrdinates.left
-          }}
-        />
-        <img src={mapFilePath} alt="Trail Map" />
-      </TransformComponent>
+      {({ zoomIn, zoomOut, setTransform, ...rest }) => (
+        <>
+          <button onClick={() => setTransform(
+            -252,
+            -543,
+            1
+          )}
+            style={{ position: 'fixed', top: '100px', left: '100px', zIndex: '1000' }}
+          >Go To Point</button>
+          <button onClick={() => setTransform(
+            0,
+            0,
+            1
+          )}
+            style={{ position: 'fixed', top: '80px', left: '100px', zIndex: '1000' }}
+          >Go To Scale 1, position (0,0)</button>
+          <button onClick={() => setTransform(
+            0,
+            0,
+            2
+          )}
+            style={{ position: 'fixed', top: '120px', left: '100px', zIndex: '1000' }}
+          >Go To Scale 2, position (0,0)</button>
+          <button onClick={() => setTransform(
+            0,
+            0,
+            3
+          )}
+            style={{ position: 'fixed', top: '140px', left: '100px', zIndex: '1000' }}
+          >Go To Scale 3, position (0,0)</button>
+          <TransformComponent>
+            <MapStateTracker
+              setCurrentMapState={handleMapStateUpdate}
+            />
+            {pins && pins.map((pin, index) => showPin(pin))}
+            <img src={mapFilePath} alt="Trail Map" ref={mapReference} />
+          </TransformComponent>
+        </>
+      )}
     </TransformWrapper>
   )
 }
--- components/menu-tray.js
+++ components/menu-tray.js
@@ -1,14 +1,27 @@
 import styles from '@/components/menu-tray.module.css';
 
-export default function MenuTray({ isSettingLocation, setIsSettingLocation }) {
+export default function MenuTray({ isSettingLocation, setIsSettingLocation, pins, setPins, crosshairsPosition, mapPosition }) {
 
   const toggleIsSettingLocation = () => {
     setIsSettingLocation(!isSettingLocation);
   }
 
+  const addPin = () => {
+    const { scale } = mapPosition;
+
+    setPins([
+      ...pins,
+      {
+        left: (crosshairsPosition.x - mapPosition.x) / scale,
+        top: (crosshairsPosition.y - mapPosition.y) / scale,
+        index: pins.length
+      }
+    ]);
+  }
+
   return (
     <div className={styles.container}>
-      {isSettingLocation && <button>Confirm</button>}
+      {isSettingLocation && <button onClick={addPin}>Confirm</button>}
       {isSettingLocation && <button onClick={toggleIsSettingLocation} >Cancel</button>}
 
       {!isSettingLocation && <button onClick={toggleIsSettingLocation} >Set Location</button>}

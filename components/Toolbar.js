67b128f7bfa1d3724f7a88f820171b4fe3a06560
Adam Bechtold <adamcbechtold@gmail.com>
2023-04-09 11:31:53 -0400

    so changes to files case are reflected in git

---
+++ components/Toolbar.js
@@ -0,0 +1,15 @@
+
+import styles from '@/components/Toolbar.module.css';
+
+export default function Toolbar({
+  changeToNextMap,
+  pins,
+  resetCurrentMap
+}) {
+  return (
+    <div className={styles.toolbarContainer}>
+        {pins.length > 0 ? <button onClick={resetCurrentMap}>Reset Pins</button> : <div></div>}     
+        <button onClick={changeToNextMap}>Next Map</button>
+    </div>
+  )
+}

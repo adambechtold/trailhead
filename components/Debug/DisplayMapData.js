ff8b2f08125bf3cd123d88f53701927ea6428613
Adam Bechtold <adamcbechtold@gmail.com>
2023-04-08 21:44:26 -0400

    so we can view debug statements

--- components/Debug/DisplayMapData.js
+++ components/Debug/DisplayMapData.js
@@ -2,13 +2,12 @@
 import styles from "./DisplayMapData.module.css";
 
 const displayObject = (object, name) => {
-  console.log(object)
   return (
     <div className={styles.debugItem}>
       <h4>{name}</h4>
       {Object.keys(object).map((key) => {
         return (
-          <div>
+          <div key={`${name}-${key}`}>
             {key}: {object[key]}
           </div>
         );
@@ -24,7 +23,7 @@ export default function DisplayMapData({
 }) {
 
   return (
-    <div className={styles.container}>
+    <div>
       {pins[0] && displayObject(pins[0], "Pin 1")}
       {pins[1] && displayObject(pins[1], "Pin 2")}
       {mapFunctionParameters &&  displayObject(mapFunctionParameters, "Map Function")}

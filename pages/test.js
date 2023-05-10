ccea8d1f1a9ea283578423e0f3bbd21bca3c365e
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-10 16:24:37 -0400

    so we have a separate page for testing tools

---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Test() {
+  return (
+    <>
+      <Head>
+        <title>Test</title>
+        <meta name="description" content="Tools to Test Wander" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </>
+  );
+}

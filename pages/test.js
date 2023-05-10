9c4740fd140f39c6372f8c53d6f789c1971db1db
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-10 16:37:59 -0400

    so we have two plots

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,22 @@
-import Head from "next/head";
+import Head from 'next/head';
+import styles from '@/styles/Test.module.css'
+
+import Plot from '@/components/Plot';
+
+
 
 export default function Test() {
   return (
     <>
       <Head>
         <title>Test</title>
-        <meta name="description" content="Tools to Test Wander" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='description' content='Tools to Test Wander' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
+      <div className={styles.container}>
+        <Plot />
+        <Plot />
+      </div>
     </>
   );
 }

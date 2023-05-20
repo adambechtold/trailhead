9350124a5d2a3962fcab69aed0f0623bf4700a38
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 19:06:46 -0400

    so we can test overlaying images

--- components/Plot.js
+++ components/Plot.js
@@ -1,20 +1,22 @@
 import 'leaflet/dist/leaflet.css';
 import styles from '@/components/Plot.module.css';
-import L from 'leaflet';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, LatLngBounds, ImageOverlay } from 'react-leaflet';
 
 import { getLineOfPins } from '@/utils/plot';
 import { createMapIcon } from '@/components/MapIcon';
 
-export default function Plot() {
+export default function Plot({
+  showPins,
+  showOverlay,
+}) {
 
   const showPin = (pin) => {
     return (
       <Marker position={[pin.latitude, pin.longitude]} icon={createMapIcon(pin.color)} key={`pin-${pin.index}}`}>
         <Popup>
           Pin {pin.index} <br />
-          Longitude: {pin.longitude} <br /> 
+          Longitude: {pin.longitude} <br />
           Latitude: {pin.latitude}
         </Popup>
       </Marker>
@@ -41,17 +43,27 @@ export default function Plot() {
     return [lat / pins.length, long / pins.length];
   };
 
+  const trailBounds = [[41.3539, -72.69313], [41.328, -72.6685]];
+  const trailURL = '../images/trail-map-smaller.jpeg';
+
   return (
     <MapContainer center={getAveragePosition(pins)} zoom={16} scrollWheelZoom={true} className={styles.plot}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {getLineOfPins({
+      {showPins && getLineOfPins({
         startPosition: pins[0],
         endPosition: pins[1],
         numberOfPins: 8,
       }).map(showPin)}
+      {showOverlay &&
+        <ImageOverlay
+          url={trailURL}
+          bounds={trailBounds}
+          opacity={0.7}
+          zIndex={10}
+        />}
     </MapContainer>
   )
 }

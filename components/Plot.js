0b84b428eb267d3dbdac40f9d9fadd0892c4f24d
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 12:24:11 -0400

    so the plots are centered on Timberlands

--- components/Plot.js
+++ components/Plot.js
@@ -5,7 +5,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Plot() {
   return (
-      <MapContainer center={[52.505, -0.09]} zoom={13} scrollWheelZoom={true} className={styles.plot}>
+      <MapContainer center={[41.33673, -72.68157]} zoom={13} scrollWheelZoom={true} className={styles.plot}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

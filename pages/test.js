b9a04be85455e7ff942a4f0144418d0ff0522809
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 11:42:10 -0400

    WIP: Undo Commit to Continue

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,12 @@
 import Head from 'next/head';
 import styles from '@/styles/Test.module.css'
 
-import Plot from '@/components/Plot';
+import dynamic from 'next/dynamic';
 
 
+const Plot = dynamic(() => import('@/components/Plot'), {
+  ssr: false
+});
 
 export default function Test() {
   return (
@@ -13,9 +16,9 @@ export default function Test() {
         <meta name='description' content='Tools to Test Wander' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
+
       <div className={styles.container}>
-        <Plot name='Input Coordinates' />
-        <Plot name='Output' />
+        <Plot />      
       </div>
     </>
   );

685fe193c11f44627074e909a2c52ecb70f9e3df
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 12:21:57 -0400

    so we display two simply plots using Leaflet

--- components/Plot.js
+++ components/Plot.js
@@ -5,16 +5,11 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Plot() {
   return (
-    // <div className={styles.container}>
-    <>
-      <MapContainer center={[52.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-        <div className={styles.plot}></div>
+      <MapContainer center={[52.505, -0.09]} zoom={13} scrollWheelZoom={true} className={styles.plot}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
-    </>
-    // </div>
   )
 }

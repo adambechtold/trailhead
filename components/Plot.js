fcc5575ae79a74f8ffb4de2a531bba715881b2ff
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 13:41:33 -0400

    so plotting functions can be organized into utils

--- components/Plot.js
+++ components/Plot.js
@@ -4,6 +4,8 @@ import L from 'leaflet';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+import { getLineOfPins } from '@/utils/plot';
+
 export default function Plot() {
 
   const createIconUrl = (color) => {
@@ -45,30 +47,6 @@ export default function Plot() {
     latitude: 41.3398,
     longitude: -72.68334
   }];
-
-  const getLineOfPins = ({
-    startPosition,
-    endPosition,
-    numberOfPins,
-  }) => {
-
-    if (numberOfPins < 2) {
-      throw new Error('numberOfPins must be greater than 1');
-    }
-
-    const line = [];
-    const latDiff = (endPosition.latitude - startPosition.latitude) / (numberOfPins - 1);
-    const longDiff = (endPosition.longitude - startPosition.longitude) / (numberOfPins - 1);
-    for (let i = 0; i < numberOfPins; i++) {
-      line.push({
-        index: i,
-        latitude: startPosition.latitude + (latDiff * i),
-        longitude: startPosition.longitude + (longDiff * i),
-      });
-    }
-
-    return line;
-  };
 
   const getAveragePosition = () => {
     let lat = 0;

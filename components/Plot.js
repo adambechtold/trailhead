7ded8c7ebdc2525c09f34e6fc9e71ea6646226fe
Adam Bechtold <adamcbechtold@gmail.com>
2023-05-20 12:51:07 -0400

    so we can place multiple pins and position the map

--- components/Plot.js
+++ components/Plot.js
@@ -10,19 +10,47 @@ export default function Plot() {
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
-  });
+});
+
+  const showPin = (pin) => {
+    return (
+      <Marker position={[pin.latitude, pin.longitude]} icon={mapIcon} key={`pin-${pin.index}}`}>
+        <Popup>
+          Pin {pin.index} <br />
+          Longitude: {pin.longitude} <br /> 
+          Latitude: {pin.latitude}
+        </Popup>
+      </Marker>
+    )
+  };
+
+  const pins = [{
+    index: 1,
+    latitude: 41.33673,
+    longitude: -72.68157,
+  }, {
+    index: 2,
+    latitude: 41.3398,
+    longitude: -72.68334
+  }];
+
+  const getAveragePosition = () => {
+    let lat = 0;
+    let long = 0;
+    pins.forEach((pin) => {
+      lat += pin.latitude;
+      long += pin.longitude;
+    });
+    return [lat / pins.length, long / pins.length];
+  };
 
   return (
-    <MapContainer center={[41.33673, -72.68157]} zoom={13} scrollWheelZoom={true} className={styles.plot}>
+    <MapContainer center={getAveragePosition(pins)} zoom={16} scrollWheelZoom={true} className={styles.plot}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[41.33673, -72.68157]} icon={mapIcon} >
-        <Popup>
-          Adam's House
-        </Popup>
-      </Marker>
+      {pins.map(showPin)}
     </MapContainer>
   )
 }

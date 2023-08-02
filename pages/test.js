7e6c733453b219f6a6756c9ad89d642c323e37c3
Adam Bechtold <adamcbechtold@gmail.com>
2023-08-02 11:50:38 -0400

    so the testing Plots can display various paths

--- pages/test.js
+++ pages/test.js
@@ -1,16 +1,45 @@
 import dynamic from 'next/dynamic';
-
 import styles from '@/styles/Test.module.css';
+import { getLineOfPins } from '@/utils/plot';
 
 const Plot = dynamic(() => import('@/components/Plot'), {
   ssr: false
+});
+
+const initialPins = [{
+  index: 1,
+  latitude: 41.33673,
+  longitude: -72.68157,
+}, {
+  index: 2,
+  latitude: 41.3398,
+  longitude: -72.68334
+}];
+
+const getAveragePosition = (pins) => {
+  let lat = 0;
+  let long = 0;
+  pins.forEach((pin) => {
+    lat += pin.latitude;
+    long += pin.longitude;
+  });
+  return [lat / pins.length, long / pins.length];
+};
+
+const pins = getLineOfPins({
+  startPosition: initialPins[0],
+  endPosition: initialPins[1],
+  numberOfPins: 8
 });
 
 export default function Test() {
   return (
     <div className={styles.container}>
       <div className={styles.plot}>
-        <Plot showPins={true} />
+        <Plot
+          pins={pins}
+          center={getAveragePosition(pins)}
+        />
       </div>
     </div>
   );

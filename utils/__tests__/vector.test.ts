6e8aec66177ea146cb16c0142f25325a81dc2d01
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-04 16:16:43 -0400

    so we can test scenarios in which the x and y access have different scalers

--- utils/__tests__/vector.test.ts
+++ utils/__tests__/vector.test.ts
@@ -64,16 +64,29 @@ describe("convertCoordinates", () => {
     expect(result.y).toBeCloseTo(8, 2);
   });
 
-  // real coordinates
-  test("converts real coordinates", () => {
-    const A_C1: Point = { x: -492.5, y: -797.5 };
-    const A_C2: Point = { x: -72.658888983, y: 41.333340991 };
-    const B_C1: Point = { x: -413.79196278370205, y: -713.2956400404881 };
-    const B_C2: Point = { x: -72.65872243798586, y: 41.333434986920594 };
+  // different scale for each dimension
+  test("converts coordinates with different scale for each dimension", () => {
+    const A_C1: Point = { x: 0, y: 0 };
+    const A_C2: Point = { x: 0, y: 0 };
+    const B_C1: Point = { x: 1, y: 1 };
+    const B_C2: Point = { x: 2, y: 3 };
 
-    let new_C1: Point = { x: 0, y: 0 };
-    let new_C2 = convertCoordinates(A_C1, A_C2, B_C1, B_C2, new_C1);
-    expect(new_C2.x).toBeCloseTo(-72.6965, 4);
-    expect(new_C2.y).toBeCloseTo(41.3545, 4);
+    let new_C1: Point = { x: 2, y: 2 };
+    let result = convertCoordinates(A_C1, A_C2, B_C1, B_C2, new_C1);
+    expect(result.x).toBeCloseTo(4, 2);
+    expect(result.y).toBeCloseTo(6, 2);
   });
+
+  // real coordinates
+  // test("converts real coordinates", () => {
+  //   const A_C1: Point = { x: -492.5, y: -797.5 };
+  //   const A_C2: Point = { x: -72.658888983, y: 41.333340991 };
+  //   const B_C1: Point = { x: -413.79196278370205, y: -713.2956400404881 };
+  //   const B_C2: Point = { x: -72.65872243798586, y: 41.333434986920594 };
+
+  //   let new_C1: Point = { x: 0, y: 0 };
+  //   let new_C2 = convertCoordinates(A_C1, A_C2, B_C1, B_C2, new_C1);
+  //   expect(new_C2.x).toBeCloseTo(-72.6965, 4);
+  //   expect(new_C2.y).toBeCloseTo(41.3545, 4);
+  // });
 });

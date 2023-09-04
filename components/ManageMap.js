f4c40b6bca5b6ab2d4b38bcabc3d4784b17c27d7
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-04 12:33:26 -0400

    so pins are used to calculate user location

--- components/ManageMap.js
+++ components/ManageMap.js
@@ -38,6 +38,7 @@ export default function ManageMap({
 
   const handleConfirmLocation = () => {
     updateUserLocation({
+      pins,
       callback: addPin,
     });
     toggleIsSettingLocation();

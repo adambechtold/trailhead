9c44631db550a4fa94aa0375b762d8473241bdaf
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-11 13:26:02 -0400

    so we can display both placed pins and a user position

--- components/InterpolateMap.tsx
+++ components/InterpolateMap.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
-import { Pin } from "@/types/Vector";
+import { Pin, Coordinates } from "@/types/Vector";
 import styles from "@/components/Map.module.css";
 
 // INPUT
@@ -11,26 +11,29 @@ import styles from "@/components/Map.module.css";
 type Props = {
   start: Pin;
   end: Pin;
+  userLocation?: Coordinates;
 };
 
 const MAP_URL = "/images/trailmap-timberlands-precise-1.jpeg";
 
 interface PinMarkerProps {
   pin: Pin;
+  type: "USER" | "PIN";
 }
 
 export function InterpolateMap(props: Props) {
-  const { start, end } = props;
+  const { start, end, userLocation } = props;
   const mapReference = useRef(null);
 
   const PinComponent: React.FC<PinMarkerProps> = (props) => {
-    const { pin } = props;
+    const { pin, type } = props;
     const left = pin.mapPoint.x;
     const top = -1 * pin.mapPoint.y;
+    const imgURL = type === "USER" ? "/user-location.svg" : "/map-x.svg";
 
     return (
       <img
-        src="/map-x.svg"
+        src={imgURL}
         alt="Pin"
         className={styles.mapPin}
         style={{
@@ -51,8 +54,8 @@ export function InterpolateMap(props: Props) {
       {() => (
         <>
           <TransformComponent>
-            <PinComponent pin={start} />
-            <PinComponent pin={end} />
+            <PinComponent pin={start} type={"PIN"} />
+            <PinComponent pin={end} type={"PIN"} />
             <img src={MAP_URL} alt="Trail Map" ref={mapReference} />
           </TransformComponent>
         </>

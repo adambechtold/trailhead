530622526f159deb2ab3bd5b283cc354b3a1a745
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-12 16:37:29 -0400

    so old function we don't use are hidden but preserved

---
+++ save/oldTestingData.js
@@ -0,0 +1,58 @@
+// ======================= TESTING =======================
+// parking lot
+// 41.336736849249846, -72.68162289645365
+
+// lookout
+// 41.33830117867603, -72.68210606267384
+
+// in between parking lot and lookout
+// 41.33736535834985, -72.68212145013308
+
+// top left corner of lake
+// 41.34219478336026, -72.68409832212741
+
+const locationBetweenLookoutAndParkingLot = {
+  latitude: 41.33736535834985,
+  longitude: -72.68212145013308,
+};
+
+const locationUpperLeftCorner = {
+  latitude: 41.34219478336026,
+  //longitude: -72.68409832212741
+  longitude: -72.6835,
+};
+
+// const [pins, setPins] = useState([
+//   // parking lot
+//   {
+//     index: 0,
+//     left: 445.977,
+//     top: 918.485,
+//     latitude: 41.336736849249846,
+//     longitude: -72.68162289645365,
+//   },
+//   // lookout
+//   {
+//     index: 1,
+//     left: 421.052,
+//     top: 842.589,
+//     latitude: 41.33830117867603,
+//     longitude: -72.68210606267384,
+//   },
+//   // in between parking lot and lookout (actual)
+//   {
+//     index: 3,
+//     left: 423,
+//     top: 878,
+//     latitude: 41.33736535834985,
+//     longitude: -72.68212145013308
+//   },
+//   // top left corner of lake (actual)
+//   {
+//     index: 4,
+//     left: 335,
+//     top: 630,
+//     latitude: 41.34219478336026,
+//     longitude: -72.68409832212741
+//   }
+// ]);

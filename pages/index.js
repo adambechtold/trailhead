32adac5cbd1a48a0ecaca8c27aa367cd13e7cada
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-20 11:44:44 -0400

    clean up old nextjs stuff

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-// TODO route the user based on what's stored locally.
 
 export default function App() {
   const router = useRouter();

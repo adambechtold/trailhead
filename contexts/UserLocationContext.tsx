e6d29b113e0fd2fcde1de81df6bf76556b1b4cbf
Adam Bechtold <adamcbechtold@gmail.com>
2023-09-26 16:25:10 -0400

    so we catch DeviceOrientation issues on more browsers

--- contexts/UserLocationContext.tsx
+++ contexts/UserLocationContext.tsx
@@ -132,6 +132,11 @@ export default function UserLocationContextProvider({
   function getCanWatchUserHeading() {
     if (!window) return;
     if (userHeadingState.error === "PERMISSION_DENIED") return false;
+    try {
+      if (!DeviceOrientationEvent) return false;
+    } catch (error) {
+      return false;
+    }
     return (
       typeof (DeviceOrientationEvent as any).requestPermission === "function"
     );

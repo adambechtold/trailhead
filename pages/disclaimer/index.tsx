63dca4fe0b17b29278d94a2442fec20f1712f269
Adam Bechtold <adamcbechtold@gmail.com>
2023-10-09 12:29:35 -0400

    so we fix typos in the disclaimer

--- pages/disclaimer/index.tsx
+++ pages/disclaimer/index.tsx
@@ -18,44 +18,49 @@ export default function DisclaimerPage() {
   return (
     <div className={styles.container}>
       <h1>Welcome to Trailhead</h1>
-      <p>Use Trailhead to turn pictures of trailmaps into GPS-enabled maps.</p>
+      <p>Use Trailhead to turn pictures of trail maps into GPS-enabled maps.</p>
       <div className={styles["section-container"]}>
         <h2>Warning: Saved maps can disappear</h2>
         <p>
           Maps are saved to your browser's local storage. That means they will
-          disappear if you clear your browing data.
+          disappear if you clear your browsing data.
         </p>
       </div>
       <div className={styles["section-container"]}>
         <h2>⚠️Warning: Do not rely on Trailhead for accurate navigation⚠️</h2>
         <p>
           <b>
-            Trailhead is experiemental and not guaranteed to provide accurate
+            Trailhead is experimental and not guaranteed to provide accurate
             navigation.
           </b>{" "}
           It has not been thoroughly tested and the accuracy of its information
-          depends heavily on the quality of the trailmap image you provide, the
+          depends heavily on the quality of the trail map image you provide, the
           accuracy of your GPS measurements, and the accuracy of your placement
           of pins. The app does not have a method of guaranteeing its accuracy
           and correct use.
         </p>
         <p>
           <b>
-            Use use Trailhead as a supplmental tool. Do not hike without other
+            Use use Trailhead as a supplemental tool. Do not hike without other
             navigation resources.
           </b>
         </p>
       </div>
       <div className={styles["button-container"]}>
         {!hasAgreedToUserAgreement && (
-          <Button onClick={onAgreeToUserAgreement} isElevated={false}>I UNDERSTAND</Button>
+          <Button onClick={onAgreeToUserAgreement} isElevated={false}>
+            I UNDERSTAND
+          </Button>
         )}
         {hasAgreedToUserAgreement && (
           <>
             <Button onClick={() => router.push("/navigate")} isElevated={false}>
               START NAVIGATING
             </Button>
-            <Button onClick={() => router.push("/how-to-use")} isElevated={false}>
+            <Button
+              onClick={() => router.push("/how-to-use")}
+              isElevated={false}
+            >
               HOW TO USE TRAILHEAD
             </Button>
           </>
